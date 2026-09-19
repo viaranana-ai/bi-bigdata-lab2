092ed16f4238da6c5e222a67203c447f9f13c555
Victor Arana <viaranana@ucvvirtual.edu.pe>
2026-09-19 12:45:56 -0500

    fix: ensure changeset 002 is included

--- changelog/db.changelog-master.sql
+++ changelog/db.changelog-master.sql
@@ -6,6 +6,6 @@ COMMENT 'Laboratorio 02 - BI y Big Data - UCV';
 
 --rollback DROP SCHEMA IF EXISTS workspace.bi_lab_7003135101;
 --changeset estudiante:002
-CREATE SCHEMA IF NOT EXISTS workspace.bi_staging_<tu_codigo>
+CREATE SCHEMA IF NOT EXISTS workspace.bi_staging_7003135101
 COMMENT 'Staging schema - BI and Big Data - Lab 03';
---rollback DROP SCHEMA IF EXISTS workspace.bi_staging_<tu_codigo>;
+--rollback DROP SCHEMA IF EXISTS workspace.bi_staging_7003135101;
