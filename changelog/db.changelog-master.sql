0810df133e6a4918b908a1431a45bf8d58195256
Victor Arana <viaranana@ucvvirtual.edu.pe>
2026-09-19 13:22:36 -0500

    test: modify checksum of changeset 002

--- changelog/db.changelog-master.sql
+++ changelog/db.changelog-master.sql
@@ -7,5 +7,5 @@ COMMENT 'Laboratorio 02 - BI y Big Data - UCV';
 --rollback DROP SCHEMA IF EXISTS workspace.bi_lab_7003135101;
 --changeset estudiante:002
 CREATE SCHEMA IF NOT EXISTS workspace.bi_staging_7003135101
-COMMENT 'Staging schema - BI and Big Data - Lab 03';
+COMMENT 'Staging schema modificado para prueba de checksum';
 --rollback DROP SCHEMA IF EXISTS workspace.bi_staging_7003135101;
